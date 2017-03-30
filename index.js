4b56572ad66fe5fb51d9e112deb296728748d0d5
Joe Turner <joe@oampo.co.uk>
2017-03-30 17:39:41 +0100

    Make a working setup for dotenv in production.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
-const runServer = require('./server').runServer;
 
 if (process.env.NODE_ENV === 'production') {
+    // Change the cwd to server to mimic running directly
+    process.chdir('server');
+    // Only require inside the if block so we don't run the server code twice
+    // in development
+    const runServer = require('./server').runServer;
     // Just run the server
     runServer(process.env.PORT || 8080);
 }
