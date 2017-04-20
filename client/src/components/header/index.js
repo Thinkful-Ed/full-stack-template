64c773f61849e3f9443c0204de796470054502cb
Ramon Reyes <reyesjunk@gmail.com>
2017-04-20 17:55:12 -0400

    added header icons that load when logged in

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './header.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import FaCog from 'react-icons/lib/fa/cog';
+import FaUserTimes  from 'react-icons/lib/fa/user-times';
+import { logOut } from '../../actions';
 
-export default function Header(props) {
+export function Header(props) {
+  function renderSettingsIcon(){
+    if(props.loggedInShelter){
+       return  <li><FaCog /></li>
+    }
+  }
+  function renderLogOutIcon(){
+    if(props.loggedInShelter){
+      return <li onClick={()=> handleLogOut()}><FaUserTimes  /></li>
+    }
+  }
+
+  function handleLogOut(){
+    props.dispatch(logOut());
+  }
+  
   return (
     <header>
       <div className='navbar'>
@@ -11,8 +30,16 @@ export default function Header(props) {
           <li>About</li>
           <li><Link style={{ textDecoration: 'none' }} to='/search'>Search</Link></li>
           <li><Link style={{ textDecoration: 'none' }} to='/shelters'>Shelters</Link></li>
+          {renderSettingsIcon()}
+          {renderLogOutIcon()}
         </ul>
       </div>
     </header>
   );
 }
+
+const mapPropsToState = state => ({
+  loggedInShelter: state.logIn.loggedInShelter
+})
+
+export default connect(mapPropsToState)(Header);
