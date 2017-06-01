13f3abd2967ac3f1564426da4c0021b787de3306
pham-thien <pham.thien@outlook.com>
2017-06-01 10:42:48 -0400

    fixing merge

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -24,22 +24,6 @@ export class Restaurant extends React.Component {
         <div className="recipes-container">
             <Recipe restaurantId={id} />
         </div>
-
-export function Restaurant(props) {
-  console.log(props);
-  const restaurant = props.restaurant[0]
-
-  return (
-    <div className="restaurant-page">
-      <h1>{restaurant.name}</h1>
-      <p className="rating">{restaurant.rating}</p>
-      <img src={restaurant.image_url} alt=""/>
-      <p className="price">{restaurant.price}</p>
-      <div className="address-container">
-        <p>{restaurant.location.display_address[0]}</p>
-        <p>{restaurant.location.display_address[1]}</p>
-        <p>{restaurant.display_phone}</p>
-      </div>
     )
   }
 }
