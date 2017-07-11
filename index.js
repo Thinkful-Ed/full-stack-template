72fb6f4438cc8b92bd487b4f87cfa426b2b847dc
Kyle Rogers <kylerogers334@gmail.com>
2017-07-11 14:26:06 +0000

    Remove duplicate app declaration

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
-const app = express();
-
 if (process.env.NODE_ENV === 'production') {
     // Change the cwd to server to mimic running directly
     process.chdir('server');
